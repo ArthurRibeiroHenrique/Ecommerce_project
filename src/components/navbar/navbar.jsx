67d725cbae5045c73ec0fef315657d8fbc96c1fe
ArthurRibeiroHenrique <art05henri@gmail.com>
2024-09-24 23:31:01 -0300

    Added more lines of code and explanation of it

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -13,8 +13,14 @@ const navbar = () => {
             </div>
             {/*Search Bar and Order Button will in this space*/}
             <div>
-                <div>
-                    <input type="text" placeholder='Type here' />
+                <div className='group'>
+                    {/*The meaning of the texts after the placeholder are: default width of 200px for the search bar. For small screen, 200px too and, when the user hover the search bar, its width will increase to 300px.
+                    
+                    To the second line of the Tailwind code, all the search bar will have a transition of 300ms when hovered, its borders will become rounded and its border will acquire the gray color, in a scale of 300.*/}
+                    {/*This is the button*/}
+                    <input type="text" placeholder='Search' 
+                    className='w-[200px] sm:w-[200px] group-hover:w-[300px] 
+                    transition-all duration-300 rounded-full border border-gray-300'/>
                 </div>
             </div>
         </div>
