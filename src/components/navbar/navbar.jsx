2ca4bccf67b5634c71d13279983401361b209459
ArthurRibeiroHenrique <art05henri@gmail.com>
2024-10-09 23:29:33 -0300

    Fixed search bar problem and added order button

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import Logo from '../../assets/shopping-basket-pngrepo-com.png'
+import Logo from '../../assets/shopping-basket-pngrepo-com.png';
+import {IoMdSearch} from 'react-icons/io';
 
 const navbar = () => {
   return (
@@ -21,16 +22,31 @@ const navbar = () => {
             </div>
             {/*Search Bar and Order Button will in this space*/}
             <div>
-                <div className='group'>
+                {/*The className below menas that, in extrasmall screens, the search bar will disappear, and, for small screens, it will become a block. In large screens, it will remain with the same configuration that it is innate.  */}
+                <div className='relative group hidden sm:block'>
                     {/*The meaning of the texts after the placeholder are: default width of 200px for the search bar. For small screen, 200px too and, when the user hover the search bar, its width will increase to 300px.
                     
                     To the second line of the Tailwind code, all the search bar will have a transition of 300ms when hovered, its borders will become rounded and its border will acquire the gray color, in a scale of 300.*/}
                     {/*This is the button*/}
                     <input type="text" placeholder='Search' 
-                    className='w-[200px] sm:w-[200px] group-hover:w-[300px] sm:group-hover:w-[200]
+                    className='w-[200px] sm:w-[200px] group-hover:w-[300px]
                     transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary '/>
+                    {/*The text inside the IoMdSearch, after the className, are used to let the IoMdSearch icon inside the search bar, with a height of 50% compared to the height of the search more. Furthermore, the translate-y-1/2 sets the icon aligned verticallt and, the right-3, adjusts the icon to be on the right of the search bar.*/}
+                    <IoMdSearch
+                    className='text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3'/>
                 </div>
             </div>
+            {/*Order Button
+            
+            The order button will follow the color settins begging in the primary and going to the secondary, with a total transition, with duration of 2ms, text in the white color, padding in the y with 1 point, paddinng in the x with 4 poins, full rounded design and following a flex standar, with items on the center and gap with 3 points. All this things are fit in the group caracteristics.*/}
+            <button onClick={() => alert("Ordering not avaible yet")}
+                className='bg-gradient-to-r from-primary to-secondary transition-all duration-200 text-white py-1 px-4 rounded-full flex items-center gap-3 group'>
+                <span
+                
+                >
+                    Order
+                </span>
+            </button>
         </div>
     </div>
     {/*Lower Navbar*/}
